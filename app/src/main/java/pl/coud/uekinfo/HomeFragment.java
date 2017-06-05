222bd839f2644dd0b81e9584dd80b2fb0c3d8d36
Coud <nikielprzemek@gmail.com>
2017-06-05 15:49:04 +0200

    added html parsing from uek news site

--- app/src/main/java/pl/coud/uekinfo/HomeFragment.java
+++ app/src/main/java/pl/coud/uekinfo/HomeFragment.java
@@ -6,13 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
+    private TextView news;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -22,8 +30,41 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        news = (TextView) view.findViewById(R.id.news);
+        getWebsite();
+        return view;
+    }
+
+    private void getWebsite() {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                try {
+                    Document doc = Jsoup.connect("http://uek.krakow.pl/pl/aktualnosci.html").get();
+                    Elements tytuly = doc.select("div[class=tytul]");
+                    int numer = 1;
+                    for(Element tytul : tytuly) {
+                        builder.append("\n" + numer + ". ").append(tytul.text()).append("\n");
+                        numer++;
+                    }
+
+                } catch (IOException e) {
+                    builder.append("Problem z połączeniem");
+                }
+                getActivity().runOnUiThread(new Runnable(){
+
+                    @Override
+                    public void run() {
+                        news.setText(builder.toString());
+                    }
+                });
+            }
+        }).start();
+
     }
 
 }
